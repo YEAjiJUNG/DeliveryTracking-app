3ea99643097d4eb24484d5e208068a17b6492ddc
YEAjiJUNG <happyhan96@naver.com>
2021-01-30 22:40:34 +0900

    Add delete function

--- src/_reducers/user_reducer.js
+++ src/_reducers/user_reducer.js
@@ -1,12 +1,16 @@
 import { createStore } from 'redux';
 import {
+    DELETE_USER,
     DELIVERY_USER
 } from '../_actions/types';
 
 const reducer = (state = [], action) => {
     switch(action.type){
         case DELIVERY_USER:
-            return [{ num: action.num }, ...state];
+            return [{ num: action.num, id: Date.now() }, ...state];
+        
+        case DELETE_USER:
+            return state.filter(num => num.id!== action.id);
 
     default:
         return state;
--- src/components/views/SearchPage/HistoryItem.js
+++ src/components/views/SearchPage/HistoryItem.js
@@ -3,12 +3,13 @@ import './HistoryItem.scss';
 import { connect } from 'react-redux';
 import { actionCreators } from '../../../_actions/user_action';
 
-const HistoryItem = ({num}) => {
+const HistoryItem = ({num, btnClick}) => {
     console.log("Item", num)
+
 
     return(
             <li className="Item">
-               {num}<button className="delete" >삭제</button>
+               {num}<button className="delete" onClick={btnClick}>삭제</button>
             </li>
     
     )
@@ -16,10 +17,13 @@ const HistoryItem = ({num}) => {
     
 }
 
+
 function mapDispatchToProps(dispatch, ownProps){
+    console.log("ownProps",ownProps);
     return{
-        delete : () => dispatch(actionCreators.deleteList(ownProps.id))
-    }
+        btnClick : () => dispatch(actionCreators.deleteList(ownProps.id))
+    };
 }
 
-export default connect(null, mapDispatchToProps)(HistoryItem);
+
+export default connect(null, mapDispatchToProps)(HistoryItem)
