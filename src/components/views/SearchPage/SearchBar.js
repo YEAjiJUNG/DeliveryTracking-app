1fe1d48ff36f8f81b9772b07eae49a7152a10107
YEAjiJUNG <happyhan96@naver.com>
2021-02-26 23:28:47 +0900

    api call

--- src/components/views/SearchPage/SearchBar.js
+++ src/components/views/SearchPage/SearchBar.js
@@ -1,45 +1,74 @@
 import React, { useState } from 'react';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './SearchBar.scss';
-import {FiSearch} from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
 import History from './History';
 import { actionCreators } from '../../../_actions/user_action';
 
 import { connect } from 'react-redux';
-
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const SearchBar = ({ addList }) => {
+  const [num, setNum] = useState('');
+  const [keyword, setKeyword] = useState('');
+  const [loading, setLoading] = useState(true);
 
-    const [num, setNum] = useState("");
-    const [keyword, setKeyword] = useState("");
+  useEffect(() => {
+    getCompanyList();
+  });
 
+  const onChange = (e) => {
+    setNum(e.target.value);
+  };
 
-    const onChange = (e) => {
-        setNum(e.target.value);
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addList(num);
+    setNum('');
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        addList(num)
-        setNum("");
-    }
+  const getCompanyList = async () => {
+    const Company = await axios.get('/api/company_list');
+    setLoading(false);
 
+    console.log(Company);
+  };
 
-    return(
-        <div className="Container">
-            <form onSubmit={onSubmit}>  
-            <input type="text" value={num} className="inputContainer" onChange={onChange} placeholder="운송장 번호 입력"></input>
-            <button type="submit" className="btn" ><FiSearch /></button>
-            </form>
-            
-        </div>
-    )
-    
+  return (
+    <div>
+      <div className="company">
+        {loading ? (
+          <div>
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div>dd</div>
+        )}
+      </div>
+      <div className="Container">
+        <div></div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            value={num}
+            className="inputContainer"
+            onChange={onChange}
+            placeholder="운송장 번호 입력"
+          ></input>
+          <button type="submit" className="btn">
+            <FiSearch />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addList: (num) => dispatch(actionCreators.addList(num)),
+  };
 }
-  
-  function mapDispatchToProps(dispatch){
-    return {
-        addList: (num) => dispatch(actionCreators.addList(num))
-    }}
 
-export default connect(null, mapDispatchToProps)(SearchBar)
+export default connect(null, mapDispatchToProps)(SearchBar);
