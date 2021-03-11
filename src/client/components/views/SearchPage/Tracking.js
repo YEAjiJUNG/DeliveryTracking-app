5c66e44f0bdcaef1382b057f2d73598491dd6160
YEAjiJUNG <happyhan96@naver.com>
2021-03-11 16:56:04 +0900

    Add tracking component

---
+++ src/client/components/views/SearchPage/Tracking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+function Tracking({ data }) {
+  return (
+    <div>
+      <h2>위치</h2>
+      <div>{}</div>
+    </div>
+  );
+}
+
+export default withRouter(Tracking);
